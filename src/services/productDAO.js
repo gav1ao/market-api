d2b9028ed21af093a979cd664b5d9f27fb5bd0a3
Vítor Gavião <vigal.jan@gmail.com>
2019-11-12 21:45:23 -0300

    Correção da query de busca de produtos
    - Correção para retornar o último registro por produto e por estabelicimento;
    
    - A query de retorno de menor preço por produto já está pronta;

--- src/services/productDAO.js
+++ src/services/productDAO.js
@@ -1,11 +1,78 @@
+const crypto = require('crypto');
+
 const Invoice = require('../models/Invoice');
 const Product = require('../models/Product');
 
+const md5 = (string) => {
+    return crypto.createHash('md5').update(string).digest('hex');
+}
+
+const removeDuplicates = (products) => {
+
+    const uniqueItens = [];
+    const productsUnique = [];
+
+    for (let i = 0; i < products.length; i++){
+        const prod = products[i];
+        
+        const item = {
+            name : prod.name,
+            marketName : prod.marketName
+        }
+
+        const prodHash = md5(JSON.stringify(item));
+
+        if (uniqueItens.indexOf(prodHash) == -1) {
+            uniqueItens.push(prodHash);
+            productsUnique.push(prod);
+        }
+    }
+
+    return productsUnique;
+}
+
 const getProductsByName = async (productName) => {
-    const products = await Product.find({ $text: { $search: productName } });
+    // TODO: Tratar caso de supermercados homônimos
+    /*const products = await Product.aggregate([
+        { $match: {
+            $text: { $search: productName } }
+        },
+        { $sort:
+            {
+                price: 1,
+                purchaseDate: -1
+            }
+        },
+        { $group:
+            {
+                _id: "$name",
+                price: { $first: "$price" },
+                marketName: { $first: "$marketName"},
+                purchaseDate: {$first: "$purchaseDate"},
+            }
+        },
+        { $sort: { price: 1} }
+    ]);*/
+
+    // TODO: Tratar caso de supermercados homônimos
+    const products = await Product.aggregate([
+        { $match: {
+            $text: { $search: productName } }
+        },
+        { $sort:
+            {
+                name: 1,
+                marketName: 1,
+                purchaseDate: -1
+            }
+        },
+    ]);
+
+    const productsFiltered = removeDuplicates(products);
+
     const productsArr = [];
 
-    products.map((prod) => {
+    productsFiltered.map((prod) => {
         const product = {
             id: prod._id,
             name: prod.name,
@@ -17,7 +84,6 @@ const getProductsByName = async (productName) => {
         productsArr.push(product);
     });
 
-    console.log(productsArr);
     return productsArr;
 }
 
