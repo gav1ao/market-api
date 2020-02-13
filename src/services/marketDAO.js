33977a8da641a62473bdbfb1e86d0d72b916d475
Vítor Gavião <vigal.jan@gmail.com>
2020-02-12 23:33:40 -0300

    Ajustes no endpoint de retorno das cidades disponíveis
    - Esse commit corrige o problema da ausência da ordenação nos nomes das cidades disponíveis;
    
    - Além disso, efetua a remoção dos dados duplicados devido a diferentes cases;

--- src/services/marketDAO.js
+++ src/services/marketDAO.js
@@ -2,14 +2,41 @@ const Invoice = require('../models/Invoice');
 
 const getMarketCities = async () => {
 
-    let cities = await Invoice.aggregate([{
-        $group : {
-            '_id': '$market.address.municipality',
-            'state': { $last : '$market.address.state' }
-        }
-    }]);
+    let cities = await Invoice.aggregate([
+        { 
+            $group : {
+                '_id': '$market.address.municipality',
+                'state': { $last : '$market.address.state' }
+            },
+        },
+        {
+            $sort: {
+                _id: 1,
+                state: 1,
+            },
+        },
+    ]);
 
-    cities = cities.filter(city => city._id !== null);
+    cities = cities
+                .filter(city => city._id !== null)
+                .map(city => {
+                    return {
+                        '_id': city._id.toUpperCase(),
+                        'state' : city.state.toUpperCase(),
+                    }
+                })
+                .reduce( (arr, curr) => {
+                    const previousCity = arr[arr.length - 1];
+
+                    if (arr.length === 0
+                        || previousCity._id !==  curr._id
+                        || (previousCity._id ===  curr._id && previousCity.state !== curr.state)
+                    ) {
+                        arr.push(curr);
+                    }
+
+                    return arr;
+                }, []);
 
     return cities;
 };
